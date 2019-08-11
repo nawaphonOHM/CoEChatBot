98df830231e7291d2299a987e9efa8c7480de82e
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-11 10:55:17 +0700

    BagsOfWords is completed.

--- src/BagsOfWords.py
+++ src/BagsOfWords.py
@@ -3,17 +3,23 @@ import json
 class BagsOfWords:
     bag = None
     bag_reverse = None
-    next_word = 0
-    spacial_token_dir = None
+    next_token = 0
 
     def __init__(self):
-        self.bag = {}
-        self.bag_reverse = {}
-        self.spacial_token_dir = json.load(
+        spacial_token_dir = json.load(
             open("./setting.json", "r", encoding="utf-8")
         )
+        self.bag = {}
+        self.bag_reverse = {}
     
     def getWord(self, token):
         return self.bag[token]
 
     def addWord(self, word):
+        self.bag_reverse[word] = self.next_token
+        self.bag[self.next_token] = word
+        self.next_token = self.next_token + 1
+    
+    def getToken(self, word):
+        return self.bag_reverse[word]
+
