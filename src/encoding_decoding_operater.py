18ea0b10a1ff10a055eb830f39b73569ce9cea0b
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-11 14:23:10 +0700

    Completed encoding_decoding_operater

--- src/encoding_decoding_operater.py
+++ src/encoding_decoding_operater.py
@@ -1,4 +1,4 @@
-import src.BagsOfWords as BagsOfWords
+from src.BagsOfWords import BagsOfWords
 
 def encoded_string(arrays_of_char, reference):
     if type(arrays_of_char) is not list:
@@ -15,5 +15,21 @@ def encoded_string(arrays_of_char, reference):
     new_returned_array = []
 
     for char in arrays_of_char:
-        
+        new_returned_array.append(reference.getToken(char))
+
+    return new_returned_array
+
+
+def decoded_string(arrays_of_token, reference):
+    if type(arrays_of_token) is not list:
+        raise TypeError(
+                "Expected arrays_of_token as list but got as {0}"
+                .format(type(arrays_of_token))
+            )
+    if type(reference) is not BagsOfWords:
+        raise TypeError(\
+                "Expected reference as BagsOfWords but got as {0}"
+                .format(type(reference))
+            )
+
     
