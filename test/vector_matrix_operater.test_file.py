c62aed3fac1d09e148551b478b41d367b42372ce
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-12 11:07:51 +0700

    Added test case

--- test/vector_matrix_operater.test_file.py
+++ test/vector_matrix_operater.test_file.py
@@ -1,12 +1,45 @@
-from src.encoding_decoding_operater import *
-from src.BagsOfWords import BagsOfWords
+from src import BagsOfWords
+from src import vector_matrix_operater
 
 bag = BagsOfWords()
 
-b = encoded_string([["EOS"], ["PAD", "EOS"]], bag)
+a = [["EOS"], ["PAD", "EOS"]]
+b = []
 
+print("First Data is ", end="")
+print(a)
 
+# Test encoded_string working
+for aa in a:
+    b.append(
+        vector_matrix_operater.encoded_string(aa, bag)
+    )
+print("After encoded is ", end="")
 print(b)
-# print(decoded_string(b, bag))
-# print(decoded_string(b, "bag"))
+
+# Test decoded_string working
+c = []
+
+for bb in b:
+    c.append(
+        vector_matrix_operater.decoded_string(bb, bag)
+    )
+
+print("After decoded is ", end="")
+print(c)
+
+# Test counting working
+for bb in b:
+    print("count ", end="")
+    print(bb, end=" ")
+    print("excluded '0' gets result as " + \
+            str(vector_matrix_operater.counting(bb, 0))
+        )
+
+# Test binary_matrix woring
+for cc in c:
+    print("binary of ", end="")
+    print(cc, end=" ")
+    print("excluded 'PAD' gets results as ", end="")
+    print(vector_matrix_operater.binary_matrix(cc, bag, "PAD"))
 
