b7dd8f083cbc1eed126ef9f4507e7437755ee9a0
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-14 12:14:31 +0700

    Edited vector to tensor

--- src/operaters/loss_function_operater.py
+++ src/operaters/loss_function_operater.py
@@ -2,25 +2,25 @@ import torch
 import torch.tensor as Tensor
 
 def mask_negative_log_likelihood_loss(\
-        input_vector,
-        target_vector,
-        mask_vector, 
+        input_tensor,
+        target_tensor,
+        mask_tensor, 
         used_device
     ):
-    if type(input_vector) is not Tensor:
+    if type(input_tensor) is not Tensor:
         raise TypeError(\
-                "Expected input_vector as Tensor but got as {0}"
-                .format(type(input_vector))
+                "Expected input_tensor as Tensor but got as {0}"
+                .format(type(input_tensor))
             )
-    if type(target_vector) is not Tensor:
+    if type(target_tensor) is not Tensor:
         raise TypeError(\
-                "Expected target_vector as Tensor but got as {0}"
-                .format(type(target_vector))
+                "Expected target_tensor as Tensor but got as {0}"
+                .format(type(target_tensor))
             )
-    if type(mask_vector) is not Tensor:
+    if type(mask_tensor) is not Tensor:
         raise TypeError(\
-                "Expected mask_vector as Tensor but got as {0}"
-                .format(type(mask_vector))
+                "Expected mask_tensor as Tensor but got as {0}"
+                .format(type(mask_tensor))
             )
     if type(used_device) is not str:
         raise TypeError(\
@@ -34,10 +34,10 @@ def mask_negative_log_likelihood_loss(\
             error_message = error_message + device + " "
         raise ValueError(error_message)
     
-    n_total = mask_vector.sum()
+    n_total = mask_tensor.sum()
     cross_entropy = -torch.log(\
-            torch.gather(input_vector, 1, target_vector.view(-1, 1)).squeeze(1)
+            torch.gather(input_tensor, 1, target_tensor.view(-1, 1)).squeeze(1)
         )
-    meaned_loss = cross_entropy.masked_select(mask_vector).mean()
+    meaned_loss = cross_entropy.masked_select(mask_tensor).mean()
     meaned_loss = meaned_loss.to(used_device)
     return meaned_loss, n_total.item()
