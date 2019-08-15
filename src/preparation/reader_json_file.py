8049cafa5b042b3fbdd3e5b545cdc967ebc183fc
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-15 17:19:20 +0700

    Changed location

--- src/preparation/reader_json_file.py
+++ src/preparation/reader_json_file.py
@@ -1,13 +1,23 @@
 import os
-import src.classes.PreProcessingDataModel as PreProcessing
+from src.classes.PreProcessingDataModel import PreProcessingDataModel as PreProcessing
 import json
 
 def read():
     raw_input = None
+    returned_data = []
 
     work_directory = os.getcwd()
     with open(os.path.join(work_directory, "data/raw/data.json"), "r") \
         as raw:
             raw_input = json.load(raw)
 
-    return raw_input
+    for input_obj in raw_input:
+        new_obj = PreProcessing()
+        new_obj.setIntension(input_obj["intension"])
+        new_obj.setInquerySentence(input_obj["inquery_sentence"])
+        new_obj.setResponseSentence(input_obj["response_sentence"])
+        new_obj.setChangedIntension(input_obj["changed_intension"])
+
+        returned_data.append(new_obj)
+
+    return returned_data
---
+++ src/processing/__init__.py
@@ -0,0 +1,1 @@
+import src.processing.transform_data
