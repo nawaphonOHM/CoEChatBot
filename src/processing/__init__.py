1f537e20de28750b72d14b75ebe7068d6a32227a
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-16 11:10:42 +0700

    Changed location file

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -2,3 +2,4 @@ from src.processing import transform_data
 from src.processing import seq2seq
 from src.processing import vector_matrix
 from src.processing import GreedySearch
+from src.processing import classes
