011582d939604dfc74a82301eca96421b6b66b30
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-21 13:57:18 +0700

    Completed Model

---
+++ src/modeling/run_model.py
@@ -0,0 +1,49 @@
+import pickle
+import os
+import numpy
+import pandas
+import pythainlp.tokenize as tokenization
+import pythainlp.corpus.common as corpus
+import src.bag_of_words as bag_of_words
+import keras.models as keras_module_manipulation
+
+def response(sentence):
+    ERROR_THRESHOLD = 0.20
+
+    work_directory = os.getcwd()
+    stop_word = corpus.thai_stopwords()
+    model = \
+        keras_module_manipulation.load_model(os.path.join(work_directory, "model/CoeChatBot.ckpt"))
+    bag = None
+    hot_coding_word = []
+    with open(os.path.join(work_directory, "model/bag_of_word_.pkl"), "rb") \
+        as model_read:
+            bag = pickle.load(model_read)
+    sentence = tokenization.word_tokenize(sentence)
+    sentence = [word for word in sentence if word not in stop_word]
+
+    for word in bag.getEntriedWords():
+        hot_coding_word.append(word in sentence)
+
+    sentence = numpy.array(hot_coding_word)
+    sentence = pandas.DataFrame([sentence], dtype=float, index=["input"])
+    sentence = model.predict([sentence])[0]
+
+    sentence = [[class_name, probability] \
+        for class_name, probability in enumerate(sentence) \
+            if probability > ERROR_THRESHOLD]
+
+    intentions = bag.getEntiredItemsIntention()
+    named_intention = intentions[sentence[0][0]]
+
+    sentence.sort(key=lambda x: x[1], reverse=True)
+
+    print(\
+            "Type: {0}; Acuracy: {1}"\
+            .format(named_intention, sentence[0][1])
+        )
+    
+    print("เจ้าหน้าที่: {0}".format(bag.getResponseSentence(named_intention)))
+
+
+response("สวัสดีครับ")
---
+++ src/modeling/trainning.py
@@ -0,0 +1,78 @@
+import tensorflow
+import random
+import numpy
+import pickle
+import os
+import json
+import pythainlp.tokenize as tokenization
+import pythainlp.corpus.common as corpus
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import src.bag_of_words as bag_of_words
+
+def train():
+    bag = None
+    work_directory = os.getcwd()
+    trainning_data = []
+    raw = None
+    stop_words = corpus.thai_stopwords()
+
+    with open(os.path.join(work_directory, "data/processed/bag_of_word_.pkl"), "rb") \
+        as bag_file:
+            bag = pickle.load(bag_file)
+    with open(os.path.join(work_directory, "data/raw/raw_data.json"), "r") \
+        as raw_data:
+            raw = json.load(raw_data)
+    
+    for json_object in raw:
+        hot_coding_pattern = []
+        hot_coding_class = []
+        query_data = json_object["inquery_sentence"]
+        class_name_data = json_object["intension"]
+        query_data = \
+            tokenization.word_tokenize(query_data, keep_whitespace=False)
+        
+        for word in bag.getEntriedWords():
+            hot_coding_pattern.append(word in query_data)
+            
+        for class_name in bag.getEntiredItemsIntention():
+            hot_coding_class.append(class_name == class_name_data)
+            
+        trainning_data.append([hot_coding_pattern, hot_coding_class])
+        
+    random.shuffle(trainning_data)
+    trainning_data = numpy.array(trainning_data)
+    features = list(trainning_data[:, 0])
+    destination_class = list(trainning_data[:, 1])
+    
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(features[0]), ), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(destination_class[0]), activation='softmax'))
+    
+    stochastic_gradient_descent = \
+        SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(\
+        loss='categorical_crossentropy', 
+        optimizer=stochastic_gradient_descent, 
+        metrics=['accuracy']
+    )
+    model.fit(\
+        numpy.array(features), 
+        numpy.array(destination_class), 
+        epochs=200, 
+        batch_size=5, 
+        verbose=1
+    )
+
+    with open(os.path.join(work_directory, "model/bag_of_word_.pkl"), "wb") \
+        as model_write:
+            pickle.dump(bag, 
+                model_write, 
+                protocol=pickle.HIGHEST_PROTOCOL
+            )
+    
+    model.save(os.path.join(work_directory, "model/CoeChatBot.ckpt"))
