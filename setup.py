fe2bff84283f441471c9622459c12cb1f6a3d8ba
nawaphonOHM <nawaphon2539@gmail.com>
2019-08-27 14:50:02 +0700

    Added changing

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+requirements = None
+
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
-
-requirements = ["numpy>=1.17.0", "tensorflow>=1.11.0", "pythainlp>=2.0.6", "pandas>=0.25.0", "keras>=2.2.5", "wheel>=0.33.6"]
+with open("./requirements.txt", "r") as requires:
+    requirements = requires.readlines()
 
 setup(
-    name="run_model",
+    name="CoEChatBotService",
     version="2.0.0",
     author="Nawaphon Isarathanachaikul",
     author_email="nawaphon2539@gmail.com",
